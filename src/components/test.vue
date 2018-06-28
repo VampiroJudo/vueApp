24d821b210e3f9a9163ebbaf32eb8b91bb9c120f
VampiroJudo <johnconley507@hotmail.com>
2018-06-28 07:48:34 -0400

    more Vue learning

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,8 +1,21 @@
   <template>
     <div class="test">
+      <input type="text" v-model="title"><br />
       <h1>{{title}}</h1>
       <p> {{ user.firstName}}</p>
-      <p>v-text="user.firstName"></p>
+      <p v-if="showName">v-text="user.firstName"></p>
+      <p v-else>Nobody</p>
+      <ul>
+          <li v-for="Item in items">{{item.title}}</li>
+      </ul>
+      <button v-on:click="greet('Hello World')">Say Greeting</button>
+      <br />
+      <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+      <hr />
+      <label>First Name: </label><input type="text" v-model="user.firstName">
+      <br />
+      <label>Last Name: </label><input type="text" v-model="user.lastName">
+      <h3>{{fullName}}</h3>
     </div>
   </template>
 
@@ -16,7 +29,28 @@
               firstName: 'John',
               lastName: 'Doe'
             },
-            active: true
+            showName: true,
+            items: [
+              {title: "Item one"},
+              {title: "Item two"},
+              {title: "Item three"}
+            ]
+          }
+        },
+        methods: {
+          greet: function() {
+            alert(greeting);
+          },
+          pressKey: function(e) {
+            console.log(e.target.value);
+          },
+          enterHit: function() {
+            console.log('You hit enter');
+          }
+        },
+        computed: {
+          fullName: function() {
+            return this.user.firstName+ ' '+this.user.lastName;
           }
         }
     }
